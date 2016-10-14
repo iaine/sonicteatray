482c75571f55c80b83c4aad66ab9bd454b8a3a84
Iain Emsley <iain.emsley@kellogg.ox.ac.uk>
2016-10-14 13:49:53 +0100

    Some initial beahviour tests

--- spec/features/steps/interaction.py
+++ spec/features/steps/interaction.py
@@ -3,23 +3,22 @@
 '''
 
 from behave import *
-from teatray import Audio
+import subprocess
 
-@given('I press the "{play}" button')
+import teatray
+
+@given("I press the {play} button")
 def step_impl(context, play):
-    raise NotImplementedError(u'STEP: Given I press the play button')
+    pass
+    #raise NotImplementedError("STEP: Given I press the " + play + "button")
 
-@when('I emit the signal "{signal}"')
+@when("I emit the signal {signal}")
 def step_impl(context, signal):
-    audio = Audio()
-    if signal == 25:
-        audio.play("DragonBite.wav")
-        assert TRUE
-    elif signal == 26:
-        audio.play("DragonBite.wav")
-        audio.pause()
+    if signal is 25:
+        print(signal)
+        subprocess.check_call(["python", "teatray "+ signal])
         assert TRUE
 
-@then(' the audio signal "{play_state}"')
+@then("the audio signal {play_state}")
 def step_impl(context, play_state):
-    raise NotImplementedError(u'STEP: Then I get an audio signal')
+    pass
