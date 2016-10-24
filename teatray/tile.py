87e434cb2694484adb356eddd8c11b007dfe70c0
Iain <iain.emsley@kellogg.ox.ac.uk>
2016-10-24 21:10:00 +0100

    Add tile handling

---
+++ teatray/tile.py
@@ -0,0 +1,14 @@
+'''
+   Tile class
+'''
+
+import audioplay
+
+class Tile():
+
+    def handle (self, tile):
+        '''
+           Handle the instance
+        '''
+        self.tile = tile
+        return tile
