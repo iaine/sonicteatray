3103fbd01ccd64f407e3d4ea452190437bd91851
Iain <iain.emsley@kellogg.ox.ac.uk>
2016-10-24 21:20:56 +0100

    Update set up

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(name='teatray',
       version='0.0.1',
       author='Iain Emsley',
       author_email='iain.emsley@kellogg.ox.ac.uk',
-      url='https://github.com/iaine/docker',
-      py_modules=['teatray', 'teatray.audioplay'],
+      url='https://github.com/iaine/sonicteatray',
+      py_modules=['teatray'],
 )
