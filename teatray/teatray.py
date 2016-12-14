f908bcba9ad706e01923a41c997e9dc304d31e75
Iain Emsley <iain.emsley@kellogg.ox.ac.uk>
2016-12-14 10:52:40 +0000

    Adding prototype code

--- teatray/teatray.py
+++ teatray/teatray.py
@@ -1,26 +1,69 @@
-'''
-   Main interface to be developed against
-'''
+#from gpiozero import Button
+#from signal import pause
+import time
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
 
-import alsaaudio
-import sys
-import wave
+import pygame.mixer
+#from pygame.mixer import Sound
+#initialise mixer
+pygame.mixer.init(frequency=44100, size=-16, channels=2, buffer=4096)
 
-from teatray.audioplay import Audio
+#initialise sounds
+print "played"
+cuckoo = pygame.mixer.Sound("home/iain/fail.wav")
+
+# initialise buttons
+padPin = 24
+GPIO.setup(padPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+padPin2 = 27
+GPIO.setup(padPin2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 
-signal = sys.argv[1]
+pad_already_pressed = False
+pad_already_pressed_2 = False
 
-if signal is None:
-    print "No signal received"
-    sys.exit()
+def motion(padPin):
+    global pad_already_pressed
+    if not pad_already_pressed:
+        pygame.mixer.music.load("/home/iain/wheatear.mp3") 
+        pygame.mixer.music.play()
+        print "Motion detected on " + str(padPin)
+        pad_already_pressed = True
 
-player = Audio()
-card = None
+
+def stopmotion(padPin):
+    global pad_already_pressed_2
+    if not pad_already_pressed_2:
+        pygame.mixer.music.load("/home/iain/cuckoo.mp3")
+        pygame.mixer.music.play()
+        print "Stop Motion detected on " + str(padPin)
+        pad_already_pressed_2 = True
+
+GPIO.add_event_detect(padPin, GPIO.RISING, callback=motion)
+GPIO.add_event_detect(padPin2, GPIO.RISING, callback=stopmotion)
+#while 1:
+    #button.when_pressed = tweeter.play
+try: 
+    time.sleep(1)
+    print ("Ready for some pins")
+    #while 1:
+    #    if GPIO.input(padPin) and not pad_already_pressed:
+    #        print "Some motion"
+        #    tweeter.play
+            #pad_already_pressed = True
+
+        #if not GPIO.input(padPin2) and not pad_already_pressed_2:
+        #    print "Other motion"
+        #    pad_already_pressed_2 = True
+    while 1:  
+        time.sleep(1)
+        print("resetting")
+        pad_already_pressed = False
+        pad_already_pressed_2 = False
+except KeyboardInterrupt:
+    GPIO.cleanup()
+#pause()
 
 
-if signal == "25":
-    f = wave.open('DragonBite.wav', 'rb')
-    device = alsaaudio.PCM(card=card)
-
-    player.play(device, f)
