7f68d5b8e8d41d90991acd0dea81c6e79c4f5965
Iain Emsley <iain.emsley@kellogg.ox.ac.uk>
2016-12-14 12:48:20 +0000

    Updating the interaction API tests

--- teatray/features/interaction.feature
+++ teatray/features/interaction.feature
@@ -3,7 +3,16 @@ As a BPSP user
 I want to touch a tile
 So that I can hear the description
 
-  Scenario:
+  Scenario: I touch a tile attached the the GPIO pin 25
     Given I am silent
     When I emit the signal 25
     Then the audio signal starts
+
+    Given I am silent
+    When I emit the signal 25
+    Then the audio for "wheatear.mp3" begins
+
+  Scenario: I touch a tile attached the the GPIO pin 26 as 25 is sounding
+    Given I am playing audio
+    When I emit the signal 26
+    Then the audio for "cuckoo.mp3" begins
--- teatray/features/steps/interaction.py
+++ teatray/features/steps/interaction.py
@@ -1,13 +1,29 @@
 from behave import *
 
+from tilehandle import TileState
+
+_tmp_state = None
+asignal = None
+
 @given(u'I am silent')
 def step_impl(context):
-    raise NotImplementedError(u'I am silent')
+    t = TileState()
+    assert t.state is False
 
 @when(u'I emit the signal {signal}')
 def step_impl(context, signal):
-    raise NotImplementedError(u'STEP: When I emit the signal 25')
+    global _tmp_state, asignal
+    t = TileState()
+    asignal = t.handle_tile(int(signal))
+    _tmp_state = t.state
 
 @then(u'the audio signal starts')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Then the audio signal starts')
+    global _tmp_state
+    assert _tmp_state is True
+
+@then(u'the audio for "wheatear.mp3" begins')
+def step_impl(context):
+    global _tmp_state, asignal
+    assert _tmp_state is True
+    assert asignal == "wheatear.mp3"
