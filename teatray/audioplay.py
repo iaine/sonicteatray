85bfc9b868429c1581aa23b77cbb7e7f2cc55a7f
Iain Emsley <iain.emsley@kellogg.ox.ac.uk>
2016-12-14 15:59:13 +0000

    Pause function added

--- teatray/audioplay.py
+++ teatray/audioplay.py
@@ -5,8 +5,9 @@
 import pygame.mixer
 
 class Audio():
-    
+
     def __init__(self):
+        self.set_time = None
         pygame.mixer.init(frequency=44100, size=-16, channels=2, buffer=4096)
 
     def play(self, filename):
@@ -16,7 +17,19 @@ class Audio():
         if filename is None or filename == '':
             print("It fewll over")
         else:
-            pygame.mixer.music.load(filename)
-            pygame.mixer.music.play() 
-            while pygame.mixer.get_busy():
-                pygame.time.delay(100)
+            try:
+                if not pygame.mixer.music.load(filename):
+                    raise Exception ("file not foud")
+                pygame.mixer.music.play()    
+                while pygame.mixer.music.get_busy():
+                    pygame.time.delay(100)
+            except Exception, e:
+                print(e)
+
+    def pause(self):
+        '''
+           Pause the file and set the position if we need to return
+        '''
+        pygame.mixer.music.pause()
+        self.set_time = pygame.mixer.music.get_pos()
+        
