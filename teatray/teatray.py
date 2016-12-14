f6e76edcdcb02f84c79cd43d43a845ef65787f07
Iain Emsley <iain.emsley@kellogg.ox.ac.uk>
2016-12-14 15:22:56 +0000

    Shell from the prototype

--- teatray/teatray.py
+++ teatray/teatray.py
@@ -1,12 +1,10 @@
-#from gpiozero import Button
-#from signal import pause
 import time
 import RPi.GPIO as GPIO
+
 GPIO.setmode(GPIO.BCM)
 
 import pygame.mixer
-#from pygame.mixer import Sound
-#initialise mixer
+
 pygame.mixer.init(frequency=44100, size=-16, channels=2, buffer=4096)
 
 #initialise sounds
@@ -43,23 +41,13 @@ def stopmotion(padPin):
 
 GPIO.add_event_detect(padPin, GPIO.RISING, callback=motion)
 GPIO.add_event_detect(padPin2, GPIO.RISING, callback=stopmotion)
-#while 1:
-    #button.when_pressed = tweeter.play
+
 try: 
     time.sleep(1)
-    print ("Ready for some pins")
-    #while 1:
-    #    if GPIO.input(padPin) and not pad_already_pressed:
-    #        print "Some motion"
-        #    tweeter.play
-            #pad_already_pressed = True
-
-        #if not GPIO.input(padPin2) and not pad_already_pressed_2:
-        #    print "Other motion"
-        #    pad_already_pressed_2 = True
+    
     while 1:  
         time.sleep(1)
-        print("resetting")
+        
         pad_already_pressed = False
         pad_already_pressed_2 = False
 except KeyboardInterrupt:
