51709f840b37b477792bf6b0fb5c3a692b8b346c
Iain <iain.emsley@kellogg.ox.ac.uk>
2016-12-15 21:39:27 +0000

    Added self

--- teatray/audioplay.py
+++ teatray/audioplay.py
@@ -7,7 +7,7 @@ import pygame.mixer
 class Audio():
     
     def __init__(self):
-        pygame.mixer.init(frequency=44100, size=-16, channels=2, buffer=4096)
+        self.pygame.mixer.init(frequency=44100, size=-16, channels=2, buffer=4096)
 
     def play(self, filename):
         '''
@@ -16,7 +16,7 @@ class Audio():
         if filename is None or filename == '':
             print("It fewll over")
         else:
-            pygame.mixer.music.load(filename)
-            pygame.mixer.music.play() 
+            self.pygame.mixer.music.load(filename)
+            self.pygame.mixer.music.play() 
             while pygame.mixer.get_busy():
-                pygame.time.delay(100)
+                self.pygame.time.delay(100)
