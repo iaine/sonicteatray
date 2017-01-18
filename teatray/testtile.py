90b5d3e33b43c17b44934c84830f273adfa42877
Iain Emsley <iain.emsley@kellogg.ox.ac.uk>
2017-01-18 18:08:44 +0000

    Tests for tiles

--- teatray/testtile.py
+++ teatray/testtile.py
@@ -1,5 +1,15 @@
 from nose.tools import *
+import os
 from tilehandle import TileState
+from dao import DAO
+
+def setup_db_func():
+    d = DAO()
+    print "inserttingdata"
+    d.insert_data([(25, "wheatear.mp3"), (26, "cuckoo.mp3")])
+
+def teardown_db_func():
+    os.remove('teatraystore.db')
 
 def test_tile_init():
     t = TileState()
@@ -10,13 +20,19 @@ def test_tile_state_change():
     rec = t.handle_tile(25)
     assert_true(t.state)
 
+@with_setup(setup_db_func)
 def test_tile_handle():
     t = TileState()
     rec = t.handle_tile(25)
-    assert rec == "wheatear.mp3"
+    assert_true(rec == "wheatear.mp3")
 
+#@with_setup(teardown_db_func)
 def test_tile_handle_false():
     t = TileState()
     rec = t.handle_tile(25)
-    assert rec != "cuckoo.mp3"
+    assert_false(rec == "cuckoo.mp3")
 
+@raises(Exception)
+def test_tile_empty_handle():
+    t = TileState()
+    rec = t.handle_tile()
