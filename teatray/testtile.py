bcd06c334e5ca9c2cf6d754dc70755ba81de72fb
Iain Emsley <iain.emsley@kellogg.ox.ac.uk>
2017-04-12 17:51:39 +0100

    Adding in command holding

--- teatray/testtile.py
+++ teatray/testtile.py
@@ -2,7 +2,7 @@ from nose.tools import *
 import os
 from tilehandle import TileState
 import sqlite3
-import time
+from datetime import datetime
 
 def setup_db_func():
     c = sqlite3.connect('teatraystore.db')
@@ -16,10 +16,28 @@ def test_tile_init():
     t = TileState()
     assert_false(t.state)
 
+def test_overview_init():
+    t = TileState()
+    assert_false(t.overview)
+
+def test_time_init():
+    t = TileState()
+    assert_true(t.cmd_time == 0)
+
 def test_tile_state_change():
     t = TileState()
     rec = t.handle_tile(25)
     assert_true(t.state)
+
+def test_tile_overview_change():
+    t = TileState()
+    rec = t.handle_tile(30)
+    assert_true(t.overview)
+
+def test_tile_time_change():
+    t = TileState()
+    rec = t.handle_tile(30)
+    assert_almost_equal(t.cmd_time, datetime.now(), 1000) 
 
 @with_setup(setup_db_func, teardown_db_func)
 def test_tile_handle():
--- teatray/tilehandle.py
+++ teatray/tilehandle.py
@@ -2,12 +2,17 @@
    Handle the tile and mappings
 '''
 from dao import DAO
+from datetime import datetime
 
 class TileState():
     state = None
+    overview = None
+    cmd_time = 0
 
     def __init__(self):
         self.state = False
+        self.overview = False
+        self.cmd_time = 0
 
     def handle_tile(self, tile_num):
         '''
@@ -15,6 +20,9 @@ class TileState():
         '''
         #change the state
         self.state = True
+        if tile_num == 30: 
+            self.overview = True
+            self.cmd_time = datetime.now()
 
         #return the type of MP3 by the tile
         return self._get_file_for_tile(tile_num)
