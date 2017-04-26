a5f84444985bba639f360efcdaad967f4d7795dc
Iain Emsley <iain.emsley@kellogg.ox.ac.uk>
2017-04-26 14:14:57 +0100

    refactoring to remove boiler plate

--- teatray/teatray.py
+++ teatray/teatray.py
@@ -1,46 +1,53 @@
 import time
 import RPi.GPIO as GPIO
+from tilehandle import TileState
+
+tile = TileState()
 
 GPIO.setmode(GPIO.BCM)
 
-import pygame.mixer
+#import pygame.mixer
 
-pygame.mixer.init(frequency=44100, size=-16, channels=2, buffer=4096)
+#pygame.mixer.init(frequency=44100, size=-16, channels=2, buffer=4096)
 
 #initialise sounds
-print "played"
-cuckoo = pygame.mixer.Sound("home/iain/fail.wav")
+#print "played"
+#cuckoo = pygame.mixer.Sound("home/iain/fail.wav")
 
 # initialise buttons
-padPin = 24
-GPIO.setup(padPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+#padPin = 24
+#GPIO.setup(padPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-padPin2 = 27
-GPIO.setup(padPin2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
+#padPin2 = 27
+#GPIO.setup(padPin2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+pins= [24,27]
+for pin in pins:
+    GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.add_event_detect(padPin, GPIO.RISING, callback=motion)
 
 pad_already_pressed = False
-pad_already_pressed_2 = False
+#pad_already_pressed_2 = False
 
 def motion(padPin):
-    global pad_already_pressed
-    if not pad_already_pressed:
-        pygame.mixer.music.load("/home/iain/wheatear.mp3") 
-        pygame.mixer.music.play()
-        print "Motion detected on " + str(padPin)
-        pad_already_pressed = True
+    tile.handle(padPin)
+#    global pad_already_pressed
+#    if not pad_already_pressed:
+#        pygame.mixer.music.load("/home/iain/wheatear.mp3") 
+#        pygame.mixer.music.play()
+#        print "Motion detected on " + str(padPin)
+#        pad_already_pressed = True
 
 
-def stopmotion(padPin):
-    global pad_already_pressed_2
-    if not pad_already_pressed_2:
-        pygame.mixer.music.load("/home/iain/cuckoo.mp3")
-        pygame.mixer.music.play()
-        print "Stop Motion detected on " + str(padPin)
-        pad_already_pressed_2 = True
+#def stopmotion(padPin):
+#    global pad_already_pressed_2
+#    if not pad_already_pressed_2:
+#        pygame.mixer.music.load("/home/iain/cuckoo.mp3")
+#        pygame.mixer.music.play()
+#        print "Stop Motion detected on " + str(padPin)
+#        pad_already_pressed_2 = True
 
 GPIO.add_event_detect(padPin, GPIO.RISING, callback=motion)
-GPIO.add_event_detect(padPin2, GPIO.RISING, callback=stopmotion)
+#GPIO.add_event_detect(padPin2, GPIO.RISING, callback=stopmotion)
 
 try: 
     time.sleep(1)
